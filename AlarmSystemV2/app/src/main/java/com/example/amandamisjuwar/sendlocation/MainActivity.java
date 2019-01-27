a2ff21678c4c22ee62be34e404c55826f7d9a9d0
Jessica Li <jessicabli0225@gmail.com>
2019-01-27 11:36:07 -0500

    fixed layout

--- AlarmSystemV2/app/src/main/java/com/example/amandamisjuwar/sendlocation/MainActivity.java
+++ AlarmSystemV2/app/src/main/java/com/example/amandamisjuwar/sendlocation/MainActivity.java
@@ -9,13 +9,51 @@ import android.app.PendingIntent;
 import android.support.v4.app.ActivityCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.volley.Request.Method;
+import android.volley.RequestQueue;
+import android.volley.Response;
+import android.volley.VolleyError;
+import android.volley.toolbox.StringRequest;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 
 public class MainActivity extends AppCompatActivity  {
 
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
-  
+
+    Timer timer=null;
+    int time=0;
+    int sensor=0;
+    double startTime;
+    double endTime;
+    boolean draw=false;
+
+    // Instantiate the RequestQueue.
+    RequestQueue queue = Volley.newRequestQueue(this);
+    String url ="http://www.google.com";
+
+    // Request a string response from the provided URL.
+    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    // Display the first 500 characters of the response string.
+                    mTextView.setText("Response is: "+ response.substring(0,500));
+                }
+            }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            mTextView.setText("That didn't work!");
+        }
+    });
+
+// Add the request to the RequestQueue.
+queue.add(stringRequest);
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +69,7 @@ public class MainActivity extends AppCompatActivity  {
         // hardcoding the number we need to send emergency text to
         String phoneNumber = "2266066008";
         // making the text that is sent
-        String message = "ALERT! CARGO THEFT DETECTED";
+        String message = "ALERT! CARGO THIEF DETECTED";
         // Use SmsManager.
         SmsManager smsManager = SmsManager.getDefault();
         String scAddress = null;
